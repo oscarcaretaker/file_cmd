ba7f474a69f58c04b9392513de850938aeff2fc6
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-03-20 21:09:05 +0530

    e

--- practie.py
+++
@@ -1,7 +0,0 @@
-def is_html(file_path):
-    with open(file_path, "rb") as f:
-        data = f.read(200).lower()  # read first part
-
-    return b"<html" in data or b"<!doctype html" in data
-x = is_html("/home/thatguyaman/Desktop/os/assigment_2/html.html")
-print(x)
